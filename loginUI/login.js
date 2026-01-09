ea37b4dc450bca0c9944d344e9b52a9b37170b6f
Ramakrishnan777 <jaihydraengineers@gmail.com>
2026-01-09 23:37:24 +0530

    FIX: added comments on HTML AND JS code

--- loginUI/login.js
+++ loginUI/login.js
@@ -1,6 +1,7 @@
 //otp input box logic
 let otpinput=document.querySelectorAll(".otpinput")
-let allowredirect=true;
+let allowredirect=true;//Flag to prevent focus() calls from re-triggering focus logic
+
    otpinput[0].focus();
 otpinput.forEach((input,index)=>{
     input.addEventListener("focus",()=>{
@@ -12,10 +13,11 @@ otpinput.forEach((input,index)=>{
         }
     }
     });
-
+//
 input.addEventListener("input",()=>{
     input.value=input.value.replace(/[^0-9]/g,"")
     if(input.value&&index<otpinput.length-1){ //cursor moves to next OTP input
+      // Prevent focus event loop
         allowredirect=false;
         otpinput[index+1].focus()
         allowredirect=true;
@@ -23,6 +25,8 @@ input.addEventListener("input",()=>{
     
 });
 input.addEventListener("keydown",(event)=>{
+      // If Backspace is pressed on empty input,
+        // move cursor to previous OTP box
     if(event.key==="Backspace"&&!input.value&&index>0){//cursor moves backward
          allowredirect=false;
         otpinput[index-1].focus()
@@ -34,28 +38,28 @@ input.addEventListener("keydown",(event)=>{
 // otp resend timer logic
 const resendotp = document.getElementById("resendotp");
 const timer = document.getElementById("timer");
-let seconds =30;
-let intervalid;
+let seconds =30;//seconds for Resend button
+let intervalid;//it will stop seconds 
 
 function startTimer(){
      
-    resendotp.classList.add("disabled");
-    seconds=30;
+    resendotp.classList.add("disabled");// disable resend btn during countdown
+    seconds=30;//reset seconds
     timer.textContent=seconds;
     intervalid=setInterval(() => {
         seconds--;
         timer.textContent=seconds;
         if(seconds===0){
-            clearInterval(intervalid);
-            resendotp.classList.remove("disabled")
+            clearInterval(intervalid);//stops countdown timer
+            resendotp.classList.remove("disabled")//makes resend otp btn visible
            
         }
     },1000);
 }
 
 resendotp.addEventListener("click",()=>{
-    if(resendotp.classList.contains("disabled")) return;
-    startTimer();
+    if(resendotp.classList.contains("disabled")) return;// ignore click if resend is disabled
+    startTimer(); 
 });
 
 //email validation
@@ -65,12 +69,12 @@ let getotpbtn=document.getElementById("getotpbtn");
 let emailregex=/^[^\s@]+@[^\s@]+\.[^\s@]+$/;
 let logindiv=document.getElementById("login-card");
 let otpdiv =document.getElementById("otp-card")
-getotpbtn.disabled=true;
+getotpbtn.disabled=true;//disable button when a valid email is entered
 email.addEventListener("blur",()=>{
 if(emailregex.test(email.value)){
     emailhelper.innerHTML="We’ll send a one-time password to this email";
     emailhelper.style.color="#777"
-    getotpbtn.disabled=false;
+    getotpbtn.disabled=false;//enable button when a valid email is entered
 
 }
 else{
@@ -89,7 +93,7 @@ getotpbtn.addEventListener("click",()=>{
         getotpbtn.disabled=false;
         logindiv.classList.add("hidden");
         otpdiv.classList.remove("hidden");
-        startTimer(); //  // Start the OTP countdown timer only when OTP card is displayed
+        startTimer(); // start OTP countdown timer after clicking get otp
 
         
     }
